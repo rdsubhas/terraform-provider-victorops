b08b24e70999a7a31b2c2e60344a239b074a47b2
Subhas Dandapani <rdsubhas@gmail.com>
2020-06-04 20:08:19 +0200

    Bootstrap provider

---
+++ victorops/provider.go
@@ -0,0 +1,11 @@
+package victorops
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		ResourcesMap: map[string]*schema.Resource{},
+	}
+}
